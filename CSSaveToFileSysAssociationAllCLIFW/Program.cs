8824d40e7bb5585c2d2b24bb0b3463edeb2f5c98
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-13 13:39:33 +0100

    Add project files.

---
+++ CSSaveToFileSysAssociationAllCLIFW/Program.cs
@@ -0,0 +1,34 @@
+﻿using CSSaveToFileSysAssociationFW;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSaveToFileSysAssociationAllCLIFW
+{
+    class Program
+    {
+        private static int sqlConnectionString = 0;
+        private static int outPath = 1;
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 6)
+            {
+                Console.WriteLine(args.Length);
+                Console.WriteLine("<sqlConnectionString> <outPath>");
+            }
+
+            string[] files =
+                Directory.GetFiles(args[outPath], "*.xml", SearchOption.TopDirectoryOnly);
+
+            new SaveToFileSysAssociationFW(
+            args[sqlConnectionString],
+            args[outPath]
+            ).WriteOutAc4yAssociationAll();
+        }
+    }
+}
